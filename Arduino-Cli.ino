d51d33727ab77541f62b0695aff5b154efb5cea0
Sait1461 <128920269+Sait1461@users.noreply.github.com>
2024-11-12 17:15:31 -0500

    Update Arduino-Cli.ino
    Code update

--- Arduino-Cli.ino
+++ Arduino-Cli.ino
@@ -1,5 +1,6 @@
 //This Script was designed to run with the linux command 'screen' in something 
 //simmilar to 'screen /dev/ttyACM0', replacing 'ttyACM0' with your board's Port.
+//Use "ctrl+a" and then "\" to exit screen
 const String Name="Console> ";
 const String Cursor="⦁";
 
@@ -20,6 +21,7 @@ String incomingString="";
 bool command = false;
 //VARIABLES FOR PROCESSES
 int iteration=0;
+bool Done=false;
 //COMMANDS
 const String Commands[] = {"help","clear","reboot","time"};
 
@@ -27,14 +29,14 @@ void Help() { //HELP
   Serial.println("");
   Serial.println("help\tDisplays this text.");
   Serial.println("clear\tClears the screen.");
-  Serial.println("reboot\tResets the board.");
+  Serial.println("reboot\tReboots the board.");
   Serial.println("time\tDisplay the time since boot.");
   Serial.println("");
 }
 void(* Reboot) (void) = 0;  //REBOOT
 void Clear() {Serial.print("\033c");}  //CLEAR
 void Time(){  //TIME
-  if (Input.length() > Commands[4].length() && Input.charAt(Input.indexOf("-") + 1) == 109) {//Minutes '-m'
+  if (Input.length() > Commands[4].length() && Input.charAt(Input.indexOf(45) + 1) == 109) {//Minutes '-m'
     Serial.print(millis() / 60000);Serial.println("m");
   } else {
     Serial.print(millis() / 1000);Serial.println("s");
@@ -53,36 +55,42 @@ void All(){
   }
 }
 
-void loop() {
+void COMMANDS(){
+  command=false;
+  if (Input.length()==0) {  /*If input IS empty*/
+      Serial.print("\r");
+  } else {  /*If input ISN'T empty*/
+    //CHECK IF INPUT IS ANY COMMAND
+    iteration=0;
+    while (iteration < sizeof(Commands)/sizeof(Commands[0]) && ! command) {
+      int i=0;
+      while (i < Input.length() && Input.charAt(i) == Commands[iteration].charAt(i)) {i++;}/*Checks for length and order*/
+      if (i == Commands[iteration].length() && (! Input.charAt(i) || Input.indexOf(32) == Commands[iteration].length())) {  /*If input IS '[COMMAND]' OR '[COMMAND] '(with a space)*/
+      command=true;
+      All();
+      }
+      iteration++;
+    }
+    //INVALID COMMAND
+    if ( ! command) {  /*If input ISN'T a command*/
+      Serial.println();
+      Serial.println("\"" + Input + "\" Is not a command");
+    }  
+    Input = "";  /*Reset the input after every enter*/
+  }
+  Done = false;
+  Serial.print("\033[2K" + Name + Input + Cursor + "\r");
+}
+void Console(String NAME) {
   if (Serial.available() > 0) {  //If theres anything being typed at all
     incomingString = Serial.readString();
     if (incomingString.indexOf(3) >= 0) { //CTRL-C
-      Serial.println("\033[2K" + Name + Input + "^C\r");
+      Serial.println("\033[2K" + NAME + Input + "^C\r");
       Serial.print("\r");
       Input="";
     } else if (incomingString.indexOf(13) >= 0) {  //ENTER
-      command=false;
-      Serial.println("\033[2K" + Name + Input + "\r"); /*To remove the cursor from previous line*/
-      if (Input.length()==0) {  /*If input IS empty*/
-        Serial.print("\r");
-      } else {  /*If input ISN'T empty*/
-        //CHECK IF INPUT IS ANY COMMAND
-        iteration=0;
-        while (iteration < sizeof(Commands)/sizeof(Commands[0]) && ! command) {
-          int i=0;
-          while (i < Input.length() && Input.charAt(i) == Commands[iteration].charAt(i)) {i++;}/*Checks for length and order*/
-          if (i == Commands[iteration].length() && (! Input.charAt(i) || Input.indexOf(32) == Commands[iteration].length())) {  /*If input IS '[COMMAND]]' OR '[COMMAND] '*/
-          command=true;
-          All();
-          }
-          iteration++;
-        }
-        //INVALID COMMAND
-        if ( ! command) {  /*If input ISN'T a command*/
-          Serial.println( "\"" + Input + "\" Is not a command");
-        }
-        Input = "";  /*Reset the input after every enter*/
-      }
+      Serial.print("\033[2K" + NAME + Input + "\r"); /*To remove the cursor from previous line*/
+      Done=true;
     } else if (incomingString.indexOf(127) >= 0) {  //BACKSPACE
       if (incomingString.length() > Input.length() ) {  /*If you try to delete more times than there's letters*/
         Input.remove(0,Input.length());
@@ -101,6 +109,13 @@ void loop() {
       if (Input.indexOf("\x1B[D") >= 0) {Input.remove(Input.indexOf("\x1B[D"), 3);}  /*Left*/
       if (Input.indexOf(9) >= 0) {Input.remove(Input.indexOf(9), 1);}  /*Tab*/
     }
-    Serial.print("\033[2K" + Name + Input + Cursor + "\r");
+    Serial.print("\033[2K" + NAME + Input + Cursor + "\r");
+  }
+}
+
+void loop() {
+  Console(Name);
+  if (Done) {
+    COMMANDS();
   }
 }
